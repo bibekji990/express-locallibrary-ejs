3358c27ab170d4833537a4c219adfadc9decd5f3
bibekji990 <bibekji990@gmail.com>
2018-05-02 00:10:58 +0300

    Added forms for book & bookInstance

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 // Require controller modules.
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
+
 
 
 

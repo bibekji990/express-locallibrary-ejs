5fd129d13e67270085a4535becb9bb3b2031ad9a
bibekji990 <bibekji990@gmail.com>
2018-05-03 03:24:58 +0300

    Added update and delete functionality

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -14,7 +14,7 @@ exports.author_list = function(req, res, next) {
     .exec(function (err, list_authors) {
       if (err) { return next(err); }
       //Successful, so render
-  console.log(list_authors);  
+    
   res.render('author_list', { title: 'Author List', author_list: list_authors });
     });
 
@@ -38,7 +38,10 @@ exports.author_detail = function(req, res, next) {
             var err = new Error('Author not found');
             err.status = 404;
             return next(err);
+    
         }
+        
+        console.log(results.author);
         // Successful, so render.
         res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
     });
@@ -98,16 +101,60 @@ exports.author_create_post = [
     }
 ];
 
+
 // Display Author delete form on GET.
-exports.author_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = function(req, res, next) {
+
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.author==null) { // No results.
+            res.redirect('/catalog/authors');
+        }
+        
+        console.log(results.author);
+        // Successful, so render.
+        res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+    });
+
 };
+
 
 // Handle Author delete on POST.
-exports.author_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete POST');
-};
+exports.author_delete_post = function(req, res, next) {
 
+    async.parallel({
+        author: function(callback) {
+          Author.findById(req.body.authorid).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.body.authorid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.authors_books.length > 0) {
+            // Author has books. Render in same way as for GET route.
+            res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/authors')
+            })
+        }
+    });
+};
+    
 // Display Author update form on GET.
 exports.author_update_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update GET');
